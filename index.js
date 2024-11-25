df77da70b139ebac5bd4c178bf03ae857a68c7da
xyryc <amdanikzz@gmail.com>
2024-11-26 01:53:50 +0600

    added post, get  operation

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
+
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +47,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Coffee Store Server
+
+1. Install _dotenv_ npm package:
+
+```bash
+npm i dotenv
+```
+
+2. Create a _.env_ file:
+
+```bash
+DB_USER=your_db_username
+DB_PASS=your_db_password
+```
+
+3. Run the server
+
+```bash
+nodemon index.js
+```
