77018bb5cd528696589530cc5a559b5cf72bf663
xyryc <amdanikzz@gmail.com>
2024-11-30 22:08:03 +0600

    added users api

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    const userCollection = client.db("coffeeDB").collection("users");
 
     app.get("/coffee", async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -75,6 +76,20 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // user realted apis
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      console.log("creating new user", newUser);
+      const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
 
